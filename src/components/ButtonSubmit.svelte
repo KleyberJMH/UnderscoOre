4a0e263121d96a5ecd5bc1cf2bb6e744f7c4385d
KleyberJMH <37642589+KleyberJMH@users.noreply.github.com>
2023-05-15 15:21:13 +0200

    Solución de error del boton

--- src/components/ButtonSubmit.svelte
+++ src/components/ButtonSubmit.svelte
@@ -14,7 +14,7 @@
 
 {#if promise === null}
 <button 
-on:click⁼{handleClick}
+on:click={handleClick}
 type="button" 
 class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
     Ask the command
